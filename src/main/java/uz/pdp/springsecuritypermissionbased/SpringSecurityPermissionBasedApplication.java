e34fd66455d20f43bcfa99306a614ca5279b0676
@zukhriddin8 <@zukhriddin808gmail.com>
2022-04-06 07:16:43 +0500

    spring

---
+++ src/main/java/uz/pdp/springsecuritypermissionbased/SpringSecurityPermissionBasedApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.springsecuritypermissionbased;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityPermissionBasedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringSecurityPermissionBasedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/springsecuritypermissionbased/SpringSecurityPermissionBasedApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.springsecuritypermissionbased;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSecurityPermissionBasedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
